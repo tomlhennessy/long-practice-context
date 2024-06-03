df3e7bfdbeeeb0fd7010b43fb6538a112b063519
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-03 18:01:26 +0100

    make dog and change context

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,4 +1,28 @@
-// Your code here
+class Dog {
+	constructor(name) {
+		this.name = name;
+	}
+
+	static makeJet() {
+		return new Dog('Jet')
+	}
+
+	changeName(newName) {
+		this.name = newName;
+		return this.name;
+	}
+
+	speak(word) {
+		return `${this.name} says ${word}`;
+	}
+}
+
+let dog1 = Dog.makeJet();
+console.log(dog1.name); // Jet
+console.log(dog1.speak("hello")); // Jet says hello
+console.log(dog1.changeName("Freyja")); // Freyja
+console.log(dog1.name); // Freyja
+console.log(dog1.speak("hello")); // Freyja says hello
 
 
 /*****************************************************************************/
--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,20 @@
 function changeContext(func, obj) {
-  // Your code here
+  return func.call(obj); // invoke func with obj as its context using call method
 }
+
+// Manual testing
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
