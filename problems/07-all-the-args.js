911fd80842b86e0be0443d16cb8797a866ad7731
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-04 19:37:46 +0100

    all args and call me later

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,19 @@
 function allTheArgs(func, ...args) {
-  // Your code here
+  return func.bind(null, ...args); // Bind func to always use args as its initial arguments
 }
+
+// Manual testing
+function sum(...nums) {
+  return nums.reduce((acc, num) => acc + num, 0);
+}
+
+const onePlusTwo = allTheArgs(sum, 1, 2);
+
+const onePlusTwoPlusThree = onePlusTwo(3);
+const onePlusTwoPlusFour = onePlusTwo(4);
+
+console.log(onePlusTwoPlusThree); // => 6
+console.log(onePlusTwoPlusFour); // => 7
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,4 +1,21 @@
-// Your code here
+class CallCenter {
+	constructor(name) {
+		this.name = name;
+	}
+
+	sayHello() {
+		console.log(`Hello this is ${this.name}`)
+	}
+
+	callMeLater(delay) {
+		setTimeout(() => this.sayHello(), delay);
+	} // arrow function to maintain context
+}
+
+// Manual testing
+let judy = new CallCenter("Judy");
+judy.sayHello(); // prints "Hello this is Judy"
+judy.callMeLater(1000); // waits one second then prints
 
 
 /*****************************************************************************/
